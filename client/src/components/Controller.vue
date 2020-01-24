79a1d2d652f467ef55191c87c86c891240fb58d7
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-24 13:59:20 +0000

    add split arrays logic

--- client/src/components/Controller.vue
+++ client/src/components/Controller.vue
@@ -9,9 +9,38 @@ import { eventBus } from "../main.js";
 import PlayerOne from "./PlayerOne.vue";
 import PlayerTwo from "./PlayerTwo.vue";
 export default {
-  name: "controller", 
+  name: "controller",
   props: ["cards"],
-  components: { "player-one": PlayerOne, "player-two": PlayerTwo }
+  components: {
+    "player-one": PlayerOne,
+    "player-two": PlayerTwo
+  },
+
+  data(){
+    return {
+      playerOneCards: [],
+      playerTwoCards: []
+    }
+  },
+
+  mounted(){
+    this.splitCards()
+  },
+
+  methods: {
+
+    splitCards(){
+      const numberOfCardsPerPlayer = this.cards.length / 2
+      for (let i = 0; i < numberOfCardsPerPlayer; i++) {
+        this.playerOneCards[i] = this.cards[i]
+      }
+      for (let i = numberOfCardsPerPlayer; i < this.cards.length; i++) {
+        this.playerTwoCards[i - numberOfCardsPerPlayer] = this.cards[i]
+      }
+    }
+
+
+  }
 };
 </script>
 

92796602f5794c940dd8b3ba46cac6a01d6ed70c
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-28 12:03:46 +0000

    removed issue in folder structure

--- GROUP_PROJECT_top_trumps/client/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-export const eventBus = new Vue();
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
--- GROUP_PROJECT_top_trumps/client/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer: {
-        disableHostCheck: true,
-        port: 4000,
-        public: '0.0.0.0:4000'
-    },
-    publicPath: "/"
-}
